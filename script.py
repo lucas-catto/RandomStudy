dab1cae9319310de1dd1d1a171f8ffae2f440e4e
Lucas Catto <lucascattosilva2018@Gmail.com>
2023-05-16 19:17:44 -0300

    Vou voltar pro Back-end agora, pois achei outro erro. (Assim que corrigir, continuo o Front-end).

--- script.py
+++ script.py
@@ -1,14 +1,43 @@
 '''
     Algoritmo que escolhe o que você deve estudar dentro das opções que você oferece, quando se esta indeciso
 '''
+def FrontEnd():
+    # ----------------------------------------------------
+    from time import sleep
+    # ----------------------------------------------------
+    for c in range(0, 60):
+        print(":", end="")
+        sleep(0.03)
+    print()
+    # ----------------------------------------------------
+    words1 = 'Você está indeciso(a) sobre o que estudar?'
+
+    for c in range(0, len(words1)):
+        print(words1[c], end='')
+        sleep(0.04)
+    print()
+    # ----------------------------------------------------
+    words2 = 'Eu posso te ajudar!'
+    for c in range(0, len(words2)):
+        print(words2[c], end="")
+        sleep(0.04)
+    print()
+    # ----------------------------------------------------
+    for c in range(0, 60):
+        print('=', end="")
+        sleep(0.03)
+    print()
+    # ----------------------------------------------------
+
+# ====================================================================================================
 
 # Aqui é só o back-end, vou fazer um front-end legal depois, mas o 'importante' é essa parte:
 def RandomStudy():
     from random import randint
 
-    vetor = []
+    FrontEnd()
 
-    print("=" * 60)
+    vetor = []
     
     try:
         amount = int(input("Informe a quantidade de matérias que deseja escolher: "))
@@ -40,4 +69,6 @@ def RandomStudy():
 
     return choose
 
+# ====================================================================================================
 print(RandomStudy())
+# ====================================================================================================
